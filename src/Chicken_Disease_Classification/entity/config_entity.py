1d68deda3744126e368f3b2f3be38cf8544a65d9
Yasbantsamal22 <yasbantsamal62@gmail.com.com>
2024-12-18 17:41:32 +0530

    Prepare Callbacks

--- src/Chicken_Disease_Classification/entity/config_entity.py
+++ src/Chicken_Disease_Classification/entity/config_entity.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -19,3 +20,10 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+@dataclass
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
